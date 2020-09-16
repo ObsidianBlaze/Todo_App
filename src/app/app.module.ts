c66c19469e345a0da131cc29d42a203bac4f0105
ObsidianBlaze <wijojames@gmail.com>
2020-09-16 13:20:34 +0100

    Todo Project Source Code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {RouterModule} from '@angular/router';
+import { ContentComponent } from './content/content.component';
+import { AddtodoComponent } from './addtodo/addtodo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    ContentComponent,
+    AddtodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', redirectTo: 'home', pathMatch: 'full'},
+      {path: 'login', component: LoginComponent},
+      {path: 'register', component: RegisterComponent},
+      {path: 'home', component: ContentComponent},
+      {path: 'addtodo', component: AddtodoComponent},
+      {path: '**', component: LoginComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
