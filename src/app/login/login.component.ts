e53fe98c4bbb80847bc07812d2388fc5567efccb
ObsidianBlaze <wijojames@gmail.com>
2020-09-24 22:50:23 +0100

    Authenticating a user during registration and login in.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {Route, Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userDetails = [
+    {username: 'osas', password: '12345'}
+  ];
+  logMessage: string;
+
+  constructor(private theRouter: Router) {
+  }
 
   ngOnInit(): void {
+  }
+
+  loginUser(form: NgForm): void {
+    // get the information from the form
+    const loginDetails = form.value;
+    // check the login details against the details in the database
+    for (const element of this.userDetails) {
+      if ((element.username === loginDetails.username) && (element.password === loginDetails.pwd)) {
+        this.logMessage = 'credentials are correct';
+        this.theRouter.navigate(['home']);
+        // this.theRouter.navigateByUrl('/');
+        break;
+      } else {
+        this.logMessage = 'Invalid username or password';
+      }
+    }
+    console.log(this.logMessage);
+
   }
 
 }
